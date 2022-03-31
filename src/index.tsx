a6e32fd888a92fa57bfb295185f17c3b3c033ccc
aleacevedo <ale.acevedo@live.com.ar>
2022-03-31 12:55:12 -0300

    feat: use hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { createGlobalStyle } from "styled-components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
 import Connect from "./Pages/Connect";
 import WalletConnect from "./Pages/WalletConnect";
@@ -22,13 +22,13 @@ declare global {
 ReactDOM.render(
   <>
     <GlobalStyle />
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Connect />} />
         <Route path="wallet-connect" element={<WalletConnect />} />
         <Route path="old" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </>,
   document.getElementById("root"),
 );
